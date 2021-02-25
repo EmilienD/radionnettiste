6270d29b87729e0c37fad20d10eefb721a8cf2b4
Durieu Emilien <contact@emilienduri.eu>
2021-02-26 00:42:53 +0100

    get google-chrome executable path through shell.which, add bin to package.json

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+#! /usr/bin/env node
+const shell = require('shelljs')
 const puppeteer = require('puppeteer')
 const stations = require('./radios')
 const initPageUpDownControls = require('./controls/pageUpDown')
-const { speak } = require('./utils')
+const { speak, write } = require('./utils')
 const createPlayer = require('./player/player')
 
 const start = async () => {
   speak('Chargement des stations de radio en cours.')
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/google-chrome',
-    headless: true,
-    defaultViewport: { height: 800, width: 1200 },
-    ignoreDefaultArgs: ['--mute-audio', '--hide-scrollbars'],
-  })
+  const browser = await puppeteer
+    .launch({
+      executablePath: shell.which('google-chrome').stdout,
+      headless: true,
+      defaultViewport: { height: 800, width: 1200 },
+      ignoreDefaultArgs: ['--mute-audio', '--hide-scrollbars'],
+    })
+    .catch((e) => {
+      speak('Désolé, ça marche pas, dommage.')
+      write('Failed to start puppeteer:', e)
+      process.exit()
+    })
 
-  createPlayer(browser, stations, initPageUpDownControls)
+  await createPlayer(browser, stations, initPageUpDownControls).catch((e) =>
+    write('Shit crashed:', e)
+  )
 }
 
 start()
